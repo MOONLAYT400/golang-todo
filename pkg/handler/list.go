4823afc0525fcccf58146b11a38c29d41b886be7
Anton <moonlayt400@gmail.com>
2025-04-08 21:45:51 +0300

    Lists update, delete functionality

--- pkg/handler/list.go
+++ pkg/handler/list.go
@@ -2,6 +2,7 @@ package handler
 
 import (
 	"net/http"
+	"strconv"
 	"todo"
 
 	"github.com/gin-gonic/gin"
@@ -53,14 +54,81 @@ func (h *Handler) getAllLists(c *gin.Context) {
 	})
 }
 
-func (h *Handler) getListById(c *gin.Context) {
+type getListByIdResponse struct {
+	Data todo.TodoList `json:"data"`
+}
 
+func (h *Handler) getListById(c *gin.Context) {
+	userId,err := getUserId(c)
+	if err != nil {
+		return
+	}
+
+	listId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "invalid id param")
+		return
+	}
+
+	list, err := h.services.TodoList.GetById(userId,listId)
+	if err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	c.JSON(http.StatusOK, getListByIdResponse{
+		Data: list,
+	})
 }
 
 func (h *Handler) updateList(c *gin.Context) {
+	userId,err := getUserId(c)
+	if err != nil {
+		return
+	}
 
+	listId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "invalid id param")
+		return
+	}
+
+	var input todo.UpdateListInput
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if err := h.services.TodoList.Update(userId,listId,input); err != nil {
+		newErrorResponse(c, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+c.JSON(http.StatusOK, statusResponse{
+	Status: "success",
+})
 }
 
 func (h *Handler) deleteList(c *gin.Context) {
+	userId,err := getUserId(c)
+	if err != nil {
+		return
+	}
 
+	listId, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		newErrorResponse(c, http.StatusBadRequest, "invalid id param")
+		return
+	}
+
+	deleteError := h.services.TodoList.Delete(userId,listId)
+	if deleteError != nil {
+		newErrorResponse(c, http.StatusInternalServerError, "internal server error")
+		return
+	}
+
+	c.JSON(http.StatusOK, statusResponse{
+		Status: "success",
+	})
 }
