59053e0dfbb54984459281e043805cc8625943ca
Anton <moonlayt400@gmail.com>
2025-04-08 19:02:34 +0300

    Create jwt auth on login

--- pkg/service/auth.go
+++ pkg/service/auth.go
@@ -4,11 +4,20 @@ import (
 	"crypto/sha1"
 	"fmt"
 	"os"
+	"time"
 	"todo"
 	"todo/pkg/repository"
+
+	"github.com/golang-jwt/jwt"
 )
 
 var salt = os.Getenv("HASH_SALT");
+const tokenTTL = 12 * time.Hour
+
+type tokenClaims struct {
+	jwt.StandardClaims
+	UserId int `json:"user_id"`
+}
 
 type AuthService struct {
 	repo repository.Authorization
@@ -21,6 +30,21 @@ func NewAuthService(repo repository.Authorization) *AuthService {
 func (s *AuthService) CreateUser(user todo.User) (int, error) {
 	user.Password = generatePasswordHash(user.Password)
 	return s.repo.CreateUser(user) 
+}
+
+func (s *AuthService) GenerateToken(username, password string) (string, error) {
+	//get user from DB
+	user, err := s.repo.GetUser(username, generatePasswordHash(password))
+	if err != nil {
+		return "", err
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, &tokenClaims{
+		jwt.StandardClaims{ExpiresAt: time.Now().Add(tokenTTL).Unix(),
+		IssuedAt: time.Now().Unix(),},
+		user.ID,
+	})
+
+	return token.SignedString([]byte(salt))
 }
 
 func  generatePasswordHash(password string) string {
