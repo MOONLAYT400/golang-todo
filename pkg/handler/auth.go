c5ddbc9d7d8c1036c3b3f14e291ab55773fe6071
Anton <moonlayt400@gmail.com>
2025-04-08 16:42:48 +0300

    Database and migrations

---
+++ pkg/handler/auth.go
@@ -0,0 +1,22 @@
+package handler
+
+import (
+	"net/http"
+	"todo"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+func (h *Handler) signUp (c *gin.Context) {
+	var input todo.User
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+}
+
+func (h *Handler) signIn (c *gin.Context) {
+
+}
---
+++ pkg/repository/repository.go
@@ -0,0 +1,27 @@
+package repository
+
+import (
+	"todo"
+
+	"github.com/jmoiron/sqlx"
+)
+
+type Authorization interface{
+	CreateUser(user todo.User) (int, error)
+}
+
+type TodoList interface{}
+
+type TodoItem interface{}
+
+type Repository struct {
+	Authorization
+	TodoList
+	TodoItem
+}
+
+func NewRepository(db *sqlx.DB) *Repository {
+	return &Repository{
+		Authorization: NewAuthPostgres(db),
+	}
+}
